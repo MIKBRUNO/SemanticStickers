aa662b249c751b502c4705e5684e4ed44a8693cc
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2024-07-11 13:52:17 +0000

    text_preocessor implemented

--- clip-server/clip_server.py
+++ clip-server/clip_server.py
@@ -1,17 +1,22 @@
-from transformers import AutoTokenizer, CLIPTextModelWithProjection
-from transformers import CLIPImageProcessor, CLIPVisionModelWithProjection
+from os import getenv
 import multiprocessing as mp
+import logging
+import sys
+from transformers import CLIPTokenizer, CLIPTextModelWithProjection
+from transformers import CLIPImageProcessor, CLIPVisionModelWithProjection
+import numpy as np
+from PIL import Image
+from io import BytesIO
 from redis import Redis
 from bson import dumps, loads
-from PIL import Image
 from aiohttp import ClientSession
 import asyncio
-from io import BytesIO
-from os import getenv
-import numpy as np
 
+logger = logging.getLogger(__name__)
 REDIS = getenv("REDIS_URL")
 IMAGE_QUEUE = "request:images"
+TEXT_QUEUE = "request:texts"
+TEXT_FLAG = "request:text-flag"
 REQUEST_COUNTER = "request:count"
 RESPONSE_QUEUE = "response"
 ERROR = "ERROR"
@@ -49,10 +54,8 @@ def image_processor() -> None:
 
     Handles imcoming image requests. Loads bunch of images and encodes it.
     """
-    # model = CLIPVisionModelWithProjection.from_pretrained("openai/clip-vit-large-patch14")
-    # processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-large-patch14")
-    model = CLIPVisionModelWithProjection.from_pretrained("openai/clip-vit-base-patch32")
-    processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    model = CLIPVisionModelWithProjection.from_pretrained("openai/clip-vit-large-patch14")
+    processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-large-patch14")
     while True:
         r = Redis.from_url(REDIS)
         # load up to `BATCH_SIZE` requests
@@ -88,15 +91,32 @@ def text_processor() -> None:
 
     Handles imcoming text requests. Loads bunch of texts and encodes it.
     """
-    pass
+    model = CLIPTextModelWithProjection.from_pretrained("openai/clip-vit-large-patch14")
+    tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
+    while True:
+        r = Redis.from_url(REDIS)
+        # load up to `BATCH_SIZE` requests
+        r.blpop(TEXT_FLAG)
+        bson_requests = r.hgetall(TEXT_QUEUE)
+        requests = [loads(req) for req in bson_requests.values()]
+        texts = [req['text'] for req in requests]
+        sequence_numbers = [req['seq'] for req in requests]
+        inputs = tokenizer(texts, padding=True, return_tensors='pt')
+        # some wierd torch magic idk
+        embeddings = model(**inputs).image_embeds.cpu().detach().numpy().astype(np.float32)
+        answers = [
+            dumps({"seq": seq, "code": SUCCESS, "answer": embed.tobytes()})
+            for seq, embed in zip(sequence_numbers, embeddings)
+        ]
+        r.lpush(RESPONSE_QUEUE, *answers)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     # image and text processors are independent, so they are working simultaneously
-    # img = mp.Process(target=image_processor, daemon=True)
-    # txt = mp.Process(target=text_processor, daemon=True)
-    # img.start()
-    # txt.start()
-    # img.join()
-    # txt.join()
-    image_processor()
+    img = mp.Process(target=image_processor, daemon=True)
+    txt = mp.Process(target=text_processor, daemon=True)
+    img.start()
+    txt.start()
+    img.join()
+    txt.join()
