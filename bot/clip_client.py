5e1378906bb69024ed71ea44985fd00d13bc80ca
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2024-07-11 14:49:02 +0000

    minor fix

--- bot/clip_client.py
+++ bot/clip_client.py
@@ -82,7 +82,7 @@ class CLIPClient:
 
     # Singleton
     _instance = None
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if not cls._instance:
             cls._instance = super().__new__(cls)
         return cls._instance
