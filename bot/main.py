b4f48ffd1b6d5d09a8b0694b62bf52813c8ec2ba
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2025-02-28 12:18:23 +0000

    removed questionable things

--- bot/main.py
+++ bot/main.py
@@ -3,26 +3,17 @@ import logging
 import sys
 from os import getenv
 
-from aiogram import Bot, Dispatcher, html, F
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import Command
-from aiogram.types import Message
-
-# for nice colored logs
-import coloredlogs
-coloredlogs.install()
-
 
 TOKEN = getenv('TELEGRAM_BOT_TOKEN')
 logger = logging.getLogger(__name__)
 
-# some logic is segregated beetween few routers, here they get connected
+# some logic is separated beetween few routers, here they get connected
 dp = Dispatcher()
-import management
 import stickers
 import inline
-dp.include_router(management.management_router)
 dp.include_router(stickers.stickers_router)
 dp.include_router(inline.inline_router)
 
